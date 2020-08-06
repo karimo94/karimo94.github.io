4aae16295b3cd7315774e4fcfa7b186453630ab0
Karim Oumghar <karimo94@outlook.com>
2020-08-05 22:59:17 -0700

    added checkout pay design

---
+++ payform-d2/script.js
@@ -0,0 +1,19 @@
+var subtotal = 0.00;
+var finalAmount = 0.00;
+
+function generateAmount() {
+    var cost = Math.random() * 200 + 20;
+    subtotal = cost;
+}
+
+$(document).ready(function() {
+
+    generateAmount();
+    calculateTotal();
+    $("p#subtotal").text("Subtotal: " + subtotal.toPrecision(5).toString());
+    $("p#finaltotal").text("Order total: " + finalAmount.toPrecision(5).toString());
+});
+
+function calculateTotal() {
+    finalAmount = subtotal + (subtotal * .035);
+}
