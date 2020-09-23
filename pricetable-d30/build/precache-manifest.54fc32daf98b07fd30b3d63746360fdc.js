3e0d2bb7a680fd6974abe90e26fb1a61e9f1f468
Karim Oumghar <karimo94@outlook.com>
2020-09-22 21:43:27 -0700

    added rebuilt build folder

---
+++ pricetable-d30/build/precache-manifest.54fc32daf98b07fd30b3d63746360fdc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98c47459eb71f18b04085cb50b0f0cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db4085f767805b2939d9",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5d8bc627ba8a95db9836",
+    "url": "./static/css/main.5c3316e6.chunk.css"
+  },
+  {
+    "revision": "db4085f767805b2939d9",
+    "url": "./static/js/2.2efd549b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.2efd549b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8bc627ba8a95db9836",
+    "url": "./static/js/main.5ded3102.chunk.js"
+  },
+  {
+    "revision": "25e1b468f5530ace692b",
+    "url": "./static/js/runtime-main.75ef47ca.js"
+  },
+  {
+    "revision": "eed1ddc23183a0a537247eeb75fd18a3",
+    "url": "./static/media/cappuccino.eed1ddc2.png"
+  },
+  {
+    "revision": "77ddaa704456924fe7cfca9a754d3340",
+    "url": "./static/media/croissant.77ddaa70.png"
+  },
+  {
+    "revision": "ea06928ea5dec565a5d881393a949bb6",
+    "url": "./static/media/espresso.ea06928e.png"
+  },
+  {
+    "revision": "382aa56f82d707e6d08645375434a6df",
+    "url": "./static/media/header.382aa56f.jpg"
+  },
+  {
+    "revision": "2218b44e7062d1c8b35d1fcc8cd14dfe",
+    "url": "./static/media/madeleine.2218b44e.png"
+  },
+  {
+    "revision": "a34f7ca8f2c821d5756e1ad1e1750d5c",
+    "url": "./static/media/mocha.a34f7ca8.png"
+  }
+]);
