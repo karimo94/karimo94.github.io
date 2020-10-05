684eb29c639180a06d7b2ed1ab071304dd6e09ff
Karim Oumghar <karimo94@outlook.com>
2020-10-05 01:28:56 -0700

    weather modified request headers

--- weather-d37/script.js
+++ weather-d37/script.js
@@ -38,7 +38,7 @@ function grabNowWeatherAndRender() {
     var request = new XMLHttpRequest();
     request.open('GET', currentWeatherEndpoint);
     request.setRequestHeader("Access-Control-Allow-Origin", "*");
-    //request.setRequestHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, origin, authorization, accept, client-security-token");
+    request.setRequestHeader("*");
     request.setRequestHeader('Access-Control-Allow-Credentials', true);
     request.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST');
     request.onload = function() {
@@ -73,7 +73,7 @@ function grabForecastAndRender() {
     var request = new XMLHttpRequest();
     request.open('GET', forecastEndpoint);
     request.setRequestHeader("Access-Control-Allow-Origin", "*");
-    //request.setRequestHeader("Access-Control-Allow-Headers", "*");
+    request.setRequestHeader("Access-Control-Allow-Headers", "*");
     request.setRequestHeader('Access-Control-Allow-Credentials', true);
     request.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST');
     var root = $('#sixDay');
