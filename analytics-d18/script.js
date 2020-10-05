e42b2a9608ef282e6e010a136fe44734f0aab7c4
Karim Oumghar <karimo94@outlook.com>
2020-10-05 00:55:54 -0700

    weather finalized, added recipe, modified analytics for fitness tracking

--- analytics-d18/script.js
+++ analytics-d18/script.js
@@ -1,18 +1,13 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Steps Taken',
+            data: [3256, 6578, 1089, 548, 2647, 4179],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.5)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -28,7 +23,7 @@ var myChart = new Chart(ctx, {
     options: {
         title: {
             display: true,
-            text: 'Title of analysis'
+            text: 'Cardio, Total Distance: 27km'
         },
         scales: {
             yAxes: [{
@@ -66,6 +61,71 @@ var playerChart = new Chart(ctx2, {
         title: {
             display: true,
             text: 'Player analysis'
+        }
+    }
+});
+
+var ctx3 = document.getElementById('myChart3').getContext('2d');
+var myChart3 = new Chart(ctx3, {
+    type: 'doughnut',
+    data: {
+        labels: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
+        datasets: [{
+            label: 'Hours slept',
+            data: [9, 6.5, 7, 5.5, 6, 7, 8],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.7)',
+                'rgba(54, 162, 235, .7)',
+                'rgba(255, 20, 0, .7)',
+                'rgba(98, 64, 235, .7)',
+                'rgba(42, 180, 164, .7)',
+                'rgba(255, 255, 25, .7)',
+                'rgba(255, 87, 33, .7)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Hours Spent on Sleep'
+        }
+    }
+});
+var ctx4 = document.getElementById('myChart4').getContext('2d');
+var myChart4 = new Chart(ctx4, {
+    type: 'doughnut',
+    data: {
+        labels: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
+        datasets: [{
+            label: 'Weightlifting',
+            data: [1, 0.5, 0.3, 0.2, .6, .75, 1.5],
+            backgroundColor: [
+                'red',
+                'blue',
+                'violet',
+                'yellow',
+                'grey',
+                'orange',
+                'green',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Hours Spent Weightlifting'
         }
     }
 });
--- weather-d37/script.js
+++ weather-d37/script.js
@@ -1,60 +1,180 @@
 const apiKey = 'f6021935fb17421b4b4cb04040a2c204';
 const currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=' + apiKey;
-const forecastEndpoint = 'https://api.openweathermap.org/data/2.5/forecast?q=Seattle&appid=' + apiKey;
+const forecastEndpoint = 'https://api.openweathermap.org/data/2.5/onecall?lat=47.6062&lon=-122.3321&exclude=alerts,current,minutely,hourly&appid=' + apiKey;
 const sun = '<i class="fas fa-sun" style="color: #ff9a76"></i>';
-const fog = '<i class="fas fa-smog" style="color: rgb(192, 192, 192)"></i>';
+const fog = '<i class="fas fa-smog" style="color: darkgrey"></i>';
 const rain = '<i class="fas fa-cloud-showers-heavy" style="color: rgb(0, 128, 255)"></i>';
 const partlyClouds = '<i class="fas fa-cloud-sun" style="color: #ffeadb"></i>';
 const overcast = '<i class="fas fa-cloud" style="#679b9b"></i>';
 const snows = '<i class="fas fa-snowflake" style="color: rgb(106, 90, 205)"></i>';
+const background_sun = 'clearskies.jpg';
+const background_clouds = 'cloudyfog.jpg';
+const background_rain = 'rainwet.jpg';
+const background_snow = 'snowcover.jpg';
+var fullCardBody = document.getElementById("appBody");
 var tempNowElement = document.getElementById("tempNow");
 var iconNowElement = document.getElementById("iconNow");
+var descNowElement = document.getElementById("descNow");
+var windNowElement = document.getElementById("windNow");
+var speedNowElement = document.getElementById("speedNow");
+var humNowElement = document.getElementById("humNow");
+var currentWeatherData = null;
+var forecastWeatherData = null;
+var metricFlag = true; //used for C/F options
 
-/*$(document).ready(function() {
-    let curWeatherData = grabNowWeather();
-    let nextWeatherData = grabForecast();
-    renderData(curWeatherData, nextWeatherData);
-});*/
-function refresh() {
-    const curWeatherData = grabNowWeather();
-    alert(curWeatherData);
-    //let nextWeatherData = grabForecast();
-    //renderData(curWeatherData, nextWeatherData);
+$(document).ready(function() {
+    grabNowWeatherAndRender();
+    grabForecastAndRender();
+});
+
+function refresh(event) {
+    event.preventDefault();
+    grabNowWeatherAndRender();
+    grabForecastAndRender();
 }
 
-function renderData(data1, dataArray) {
 
-}
-
-function grabNowWeather() {
-    var data = null;
-
+function grabNowWeatherAndRender() {
     var request = new XMLHttpRequest();
-    request.open('GET', currentWeatherEndpoint + apiKey);
-
-    alert(request.status);
-
+    request.open('GET', currentWeatherEndpoint);
+    request.setRequestHeader("Access-Control-Allow-Headers", "*");
+    request.setRequestHeader('Access-Control-Allow-Credentials', true);
+    request.setRequestHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     request.onload = function() {
+
         var jsonData = JSON.parse(this.response);
-        data = jsonData
-        return data;
+        currentWeatherData = jsonData;
+
+        //temp
+        tempNowElement.innerHTML = kelvinToCelsius(currentWeatherData.main.temp) + '&#176;C';
+        //condition
+        var condition = currentWeatherData.weather[0].main;
+        descNowElement.innerHTML = condition;
+        //wind & speed
+        var windNow = 'Wind: ' + currentWeatherData.wind.deg + '&#176;';
+        windNowElement.innerHTML = windNow;
+        speedNowElement.innerHTML = 'Speed: ' + Math.round(currentWeatherData.wind.speed) + 'm/s';
+        //humidity
+        var humNow = 'Hum: ' + currentWeatherData.main.humidity + '%';
+        humNowElement.innerHTML = humNow;
+        //icon to render
+        iconNowElement.innerHTML = renderConditionIcon(condition);
+        //background to render
+        var backgroundUrl = backgroundToRender(condition);
+        fullCardBody.style.backgroundImage = 'url(' + backgroundUrl + ')';
+        fullCardBody.style.backgroundSize = 'cover';
     }
 
     request.send();
-    alert(request.status);
-
-    return data;
 }
 
-function grabForecast() {
-    //placeholder
+function grabForecastAndRender() {
+    var request = new XMLHttpRequest();
+    request.open('GET', forecastEndpoint);
+    request.setRequestHeader("Access-Control-Allow-Headers", "*");
+    request.setRequestHeader('Access-Control-Allow-Credentials', true);
+    request.setRequestHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    var root = $('#sixDay');
+    root.empty(); //re-draw
+    var card = '';
+    request.onload = function() {
+
+        var jsonData = JSON.parse(this.response);
+        forecastWeatherData = jsonData;
+        /*forecastWeatherData.daily[i].temp.day - temperature
+        forecastWeatherData.daily[i].weather[0].main - description */
+        /*<div class="col-4">
+                <div class="card" id="day1"><h5>icon</h5><h5>temp</h5><h6>desc</h6></div>
+        </div>*/
+
+        //render divs with icons at the top, then temp, then description
+        for (var i = 1; i < forecastWeatherData.daily.length - 1; i += 1) {
+            var temp = kelvinToCelsius(forecastWeatherData.daily[i].temp.day) + '&#176;C';
+            var desc = forecastWeatherData.daily[i].weather[0].main;
+            var iconToRender = renderConditionIcon(desc);
+            var day = dayNumToDayOfWeek(new Date().getDay() + i);
+            card = '<div class="col-4"> <div class="card"><p>' + day + '</p><h5>' + iconToRender + '</h5><h5>' +
+                temp + '</h5><h6>' + desc + '</h6></div> </div>';
+            root.append(card);
+        }
+    }
+
+    request.send();
 }
 
 
 function kelvinToCelsius(input) {
-    return input - 273.15;
+    return Math.round(input - 273.15);
 }
 
 function kelvinToFahr(input) {
-    return (input - 273.15) * (9 / 5) + 32;
+    return Math.round((input - 273.15) * (9 / 5) + 32);
+}
+
+function renderConditionIcon(condition) {
+    //icon to render
+    if (condition === 'Clear' || condition === 'Sunny') {
+        return sun;
+    }
+    if (condition === 'Smoke' || condition === 'Fog' || condition === 'Mist') {
+        return fog;
+    }
+    if (condition === 'Clouds' || condition === 'Overcast') {
+        return overcast;
+    }
+    if (condition === 'Rain' || condition === 'Showers') {
+        return rain;
+    } else {
+        return snows;
+    }
+}
+
+function dayNumToDayOfWeek(value) {
+    let day = '';
+    //reset
+    if (value > 6) {
+        value -= 6;
+    }
+    switch (value) {
+        case 0:
+            day = 'Sunday';
+            break;
+        case 1:
+            day = 'Monday';
+            break;
+        case 2:
+            day = 'Tuesday';
+            break;
+        case 3:
+            day = 'Wednesday';
+            break;
+        case 4:
+            day = 'Thursday';
+            break;
+        case 5:
+            day = 'Friday';
+            break;
+        case 6:
+            day = 'Saturday';
+            break;
+    }
+    return day;
+}
+
+function backgroundToRender(condition) {
+    //icon to render
+    if (condition === 'Clear' || condition === 'Sunny') {
+        return background_sun;
+    }
+    if (condition === 'Smoke' || condition === 'Fog' || condition === 'Mist') {
+        return background_clouds;
+    }
+    if (condition === 'Clouds' || condition === 'Overcast') {
+        return background_clouds;
+    }
+    if (condition === 'Rain' || condition === 'Showers') {
+        return background_rain;
+    } else {
+        return background_snow;
+    }
 }
