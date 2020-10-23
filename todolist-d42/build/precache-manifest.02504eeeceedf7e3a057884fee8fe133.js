4b29095abe800d95c104784f5695ea3363aac727
Karim Oumghar <karimo94@outlook.com>
2020-10-22 21:40:21 -0700

    build folder for todolist in this commit

---
+++ todolist-d42/build/precache-manifest.02504eeeceedf7e3a057884fee8fe133.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c2d5cccc6d17ba0eb18f5c8a4febd6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "25c8b93c1e5efa798378",
+    "url": "./static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "bab3dd27286cf03695ca",
+    "url": "./static/css/main.84ade315.chunk.css"
+  },
+  {
+    "revision": "25c8b93c1e5efa798378",
+    "url": "./static/js/2.8d8591b7.chunk.js"
+  },
+  {
+    "revision": "7fa8ec16d0a9f9102dca5c04818f2334",
+    "url": "./static/js/2.8d8591b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bab3dd27286cf03695ca",
+    "url": "./static/js/main.51f8c446.chunk.js"
+  },
+  {
+    "revision": "3444346fef1b4832e4af",
+    "url": "./static/js/runtime-main.ea534bfc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
